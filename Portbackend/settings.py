61fb76a450998d7937b247d9d695e4c54b15fa79
ansarimohammed422 <ansarimohammed122@gmail.com>
2026-04-03 04:33:05 +0530

    Probabilly email fix

--- Portbackend/settings.py
+++ Portbackend/settings.py
@@ -70,6 +70,7 @@ ROOT_URLCONF = "Portbackend.urls"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://mohammed-portfolio-gilt-three.vercel.app/",
     # Add your production React domain here later
 ]
 
