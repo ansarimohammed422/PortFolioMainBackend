3cb1476473ef7a1e4646f0ec842a7e605b03dfbd
ansarimohammed422 <ansarimohammed122@gmail.com>
2026-04-08 03:29:34 +0530

    Updated Certificates and Projects

--- Portbackend/settings.py
+++ Portbackend/settings.py
@@ -72,7 +72,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://mohammed-portfolio-gilt-three.vercel.app",
-    # Add your production React domain here later
 ]
 
 TEMPLATES = [
